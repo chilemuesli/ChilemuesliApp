fc63aa06964039843f8764e1440a9f9cdbfbccb9
Marc Baumgartner <marc@mabaka.ch>
2019-11-22 21:45:45 +0100

    Project created with React Native CLI.

---
+++ ios/Pods/Target Support Files/Pods-ChilemuesliApp-tvOS/Pods-ChilemuesliApp-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChilemuesliApp_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_ChilemuesliApp_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-ChilemuesliAppTests/Pods-ChilemuesliAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChilemuesliAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_ChilemuesliAppTests
+@end
