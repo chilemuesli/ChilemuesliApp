67ab8cc5856d859f4d876ac104077bc51fe9fe8d
Marc Baumgartner <marc@mabaka.ch>
2019-12-26 21:58:11 +0100

    Remove unused code

--- App.js
+++ App.js
@@ -8,7 +8,6 @@
 import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {createStackNavigator} from 'react-navigation-stack';
 import WelcomeScreen from './Screens/WelcomeScreen';
 import BeaconsDemoScreen from './Screens/BeaconsDemoScreen';
@@ -46,52 +45,6 @@ const MainNavigator = createStackNavigator({
   },
 });
 
-const AboutNavigator = createStackNavigator({
-  Test: {
-    screen: BeaconsDemoScreen,
-    routeName: 'Test',
-  },
-});
-
-const TabNavigator = createBottomTabNavigator(
-  {
-    Home: {
-      screen: MainNavigator,
-    },
-    About: {
-      screen: AboutNavigator,
-    },
-  },
-  {
-    defaultNavigationOptions: ({navigation}) => ({
-      tabBarIcon: ({focused, horizontal, tintColor}) => {
-        const {routeName} = navigation.state;
-        let IconComponent = Ionicons;
-        let iconName;
-        if (routeName === 'Home') {
-          iconName = 'ios-home';
-          //iconName = `ios-home${focused ? '' : '-outline'}`;
-          // Sometimes we want to add badges to some icons.
-          // You can check the implementation below.
-          // IconComponent = HomeIconWithBadge;
-        } else if (routeName === 'About') {
-          iconName = 'ios-information-circle-outline';
-        } else if (routeName === 'Settings') {
-          iconName = 'ios-options';
-        } else {
-          iconName = 'ios-frowno';
-        }
-
-        // You can return any component that you like here!
-        return <IconComponent name={iconName} size={25} color={tintColor} />;
-      },
-    }),
-    tabBarOptions: {
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
-    },
-  },
-);
 const AppContainer = createAppContainer(MainNavigator);
 
 export default class App extends React.Component {
